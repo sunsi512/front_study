ac28ec2c303e51e2d64f1b7652cd886905ac5563
sunsi512 <d58230608@gmail.com>
2022-12-03 13:00:14 +0900

    221203

---
+++ html/date.js
@@ -0,0 +1,5 @@
+
+
+let today = new Date();
+
+console.log(today)
---
+++ html/variavle01.js
@@ -0,0 +1,33 @@
+
+
+// 1. 리터럴 literal
+console.log('hello');
+console.log("hello");
+console.log('hello')
+
+console.log("2022-10-10") // 문자
+console.log(2022-10-10) // 숫자
+
+// 2. 변수
+// 변수 종루 : var, let, const
+
+var num = 123;
+var greeting = 'hi'
+
+console.log(num);
+console.log(greeting);
+
+var x = 2
+var y = '2'
+
+console.log(x+x);
+console.log(y+y);
+
+
+// 3. javascript 이스케이프 시퀀스 ( \ 역슬래시를 활용한 특수문자표현)
+
+console.log('it\'s a book')
+console.log("it's a book")
+console.log("NEw York is called \"The BIg Apple\".")
+
+
